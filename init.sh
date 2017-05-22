1e53782990c8acd8df436906ecd998d372c39eef
Kevin Pedro <kpedro88@gmail.com>
2017-05-22 15:01:28 -0500

    setup paths to link to Delphes in ROOT

--- init.sh
+++ init.sh
@@ -4,6 +4,7 @@ CUR_DIR=`pwd`/..
 source /cvmfs/sft.cern.ch/lcg/views/LCG_87/x86_64-slc6-gcc49-opt/setup.sh
 export PYTHIA8=$CUR_DIR/pythia8226
 export PYTHIA8DATA=$PYTHIA8/share/Pythia8/xmldoc/
-export LD_LIBRARY_PATH=${PYTHIA8}/lib:${LD_LIBRARY_PATH}
 export DELPHES=$CUR_DIR/Delphes-3.4.1
 export PATH=$DELPHES:${PATH}
+export LD_LIBRARY_PATH=${PYTHIA8}/lib:${DELPHES}:${LD_LIBRARY_PATH}
+export ROOT_INCLUDE_PATH=${DELPHES}:${ROOT_INCLUDE_PATH}
